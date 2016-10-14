c4f0f8dba1dc724854e3b37f8a20800a39a435b3
p8queiroz <phqs.phqs@gmail.com>
2016-10-14 11:15:50 -0300

    refactoring the code

--- DeleteAllItemsSharePoint.js
+++ DeleteAllItemsSharePoint.js
@@ -1,56 +1,78 @@
-var itemsToDelete = [];
+var groupName = 'My SharePoint Site Group Members';
+var listName = 'MyListInstanceName';
 
-function getItemsToDelete(ListName) {
-	//Get All items of the list
-    var requestUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle(" + ListName + ")/items?$filter=ID gt 0"
+function breakSecurityInheritanceChangeUser(id, groupName, listName) {
 
+    var clientContext = new SP.ClientContext.get_current();
+    var oList = clientContext.get_web().get_lists().getByTitle(listName);
+    var itemId = id;
+    this.oListItem = oList.getItemById(itemId);
+
+    oListItem.breakRoleInheritance(true);
+
+
+    /*adding "administrator" role for the user and then add the user to the item */
+    var collRoleDefinitionBinding = SP.RoleDefinitionBindingCollection.newObject(clientContext);
+    collRoleDefinitionBinding.add(clientContext.get_web().get_roleDefinitions().getByType(SP.RoleType.administrator));
+
+
+	/*Get group name*/
+    var groupCollection = clientContext.get_web().get_siteGroups();
+    var group = groupCollection.getByName(groupName);
+
+
+	/*Add the role to the group and the group to the item */ 
+    oListItem.get_roleAssignments().add(group, collRoleDefinitionBinding);
+
+
+    /*load changes*/
+    clientContext.load(oListItem);
+
+    /*commit changes to the server*/
+    clientContext.executeQueryAsync(Function.createDelegate(this, function () { Success(groupName); }), Function.createDelegate(this, this.Failure));
+
+}
+
+function Success(groupName) {
+    console.log("sucess");
+}
+
+function Failure(sender, args) {
+    console.log("error");
+}
+
+
+function getAllitems(listName) {
+    /*Create custom query. It only retrieves 100 items. You can adapt the query bellow for your need*/
+    var requestUrl = _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle('" + listName + "')/items?$filter=(ID ge 1) and (ID le 99)";
+
+    //Return and ajax request (promise)
     return $.ajax({
         url: requestUrl,
         type: "GET",
         headers: {
             "accept": "application/json;odata=verbose",
         },
-        success: function(result) {
-            $.each(result.d.results, function(index, item){
-                itemsToDelete.push(deleteItem(item));
-            });            
+        success: function (result) {
+            $.each(result.d.results, function (index, item) {
+				/*Show the ID of the items retrieved*/
+				console.log("results : " + item.ID);
+                Allitems.push(breakSecurityInheritanceChangeUser(item.ID, groupName, listName));
+            });
         },
-        error: function(error) {
-            
+        error: function (error) {
+            /*Something went wrong...*/
+            console.log("error");
         }
-    });    
-}
-
-function deleteItem(item) {
-    
-    var requestUrl = item.__metadata.uri;
-
-    return $.ajax({
-        url: requestUrl,
-        type: "POST",
-        headers: {
-            "accept": "application/json;odata=verbose",
-            "X-RequestDigest": $("#__REQUESTDIGEST").val(),
-            "IF-MATCH": item.__metadata.etag,
-            "X-HTTP-Method": "DELETE"
-        },
-        success: function() {
-            console.log("Item with ID " + item.__metadata.id + " successfully deleted!");
-        },
-        error: function(error) {
-            
-        }
-    });    
-}
-
-
-
-//Process the Request getItemsToDelete
-$.when(getItemsToDelete("ListNamePlaceholder")).then(function () {
-
-	//Process the Request after getItemsToDelete finishes
-    $.when.apply($, itemsToDelete).then(function(){
-        console.log("All items are deleted!");
     });
-	
+}
+
+/*Declare an array of deferred objects that hold a breakSecurityInheritanceChangeUser request for each item that will be affected*/
+var Allitems = [];
+
+/*First get the items*/
+$.when(getAllitems(listName)).then(function () {
+    $.when.apply($, Allitems).then(function () {
+        console.log("InhehitanceBroken - security item level permission all set!");
+    });
 });
